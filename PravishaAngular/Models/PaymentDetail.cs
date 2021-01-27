092fd44725e0bf1f80cc63cf596a100b0b4cca0e
Pravisha Naipaal <pnaipaal@gmail.com>
2021-01-27 11:21:39 +0200

    Initial commit

---
+++ PravishaAngular/Models/PaymentDetail.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PravishaAngular.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(16)")]
+        public string CardNumber { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(5)")]
+        public string ExpirationDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(3)")]
+        public string SecurityCode { get; set; }
+    }
+}
